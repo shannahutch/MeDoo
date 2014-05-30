d0255eb6ca393ae57493ec6ab4c434fbf4bf9961
Githubshanna <shannahutch@gmail.com>
2014-05-30 10:13:48 -0700

    model and request spec with pry and typhoeus gem"

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe GoalsController do
   describe "GET index" do
-    it "assigns @goals" do
-      goal = Goal.create
-      get :index
-      expect(assigns(:teams)).to eq([goal])
-    end
+    # it "returns a list of goals" do
+    #   goal = Goal.create
+    #   get :index
+    #   expect(assigns(:goals)).to eq([goal])
+    # end
 
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
+    # it "renders the index template" do
+    #   get :index
+    #   expect(response).to render_template("index")
+    # end
   end
   
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Goal do
+  #testing associations
   it do
     g = Goal.reflect_on_association(:months)
     g.macro.should === :has_many
@@ -11,7 +12,26 @@ describe Goal do
   end
   it do
     g = Goal.reflect_on_association(:days)
-    g.macro.should === :has_many
+    g.macro.should === :has_many 
   end
-  
+  describe 'a new goal' do
+    it 'can be created'do
+      @goal = Goal.create(name: "max", description: "get there this year")
+
+      Goal.where(name: "max").first.should_not == nil
+# testing methods  to see that they work not content
+      @goal.should respond_to :description
+      @goal.should respond_to :name
+#testing for content
+      @goal.name.should == "max"
+      @goal.description.should == "get there this year"
+    end
+    it 'can be destroyed' do
+       @goal2 = Goal.create(name: "max", description: "get there this year")
+       @goal2.destroy
+       #Goal.where(name: "max").first.destroy
+
+       @goal2.should_not == nil
+    end
+  end
 end
