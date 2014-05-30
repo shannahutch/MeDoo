438522b9cbd1e8b2d7dfd0dfd9fc8b51d1be21b7
Githubshanna <shannahutch@gmail.com>
2014-05-29 20:40:14 -0700

    rspec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  class ActiveSupport::TestCase
+  include Devise::TestHelpers
+end
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
